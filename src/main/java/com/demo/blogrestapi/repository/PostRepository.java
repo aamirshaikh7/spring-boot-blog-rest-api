4bba0fe821e0732c97a0bb1b35b0e5224b91f324
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-04 14:17:07 +0530

    Creating Post Entity, Repository and Custom Exception - ResourceNotFoundException

---
+++ src/main/java/com/demo/blogrestapi/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.demo.blogrestapi.repository;
+
+import com.demo.blogrestapi.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
