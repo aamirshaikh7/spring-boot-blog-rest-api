53f84480cf977f8b992552409b72a47fded7d796
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-11 20:14:06 +0530

    Validating create and update Post Rest API requests

--- src/main/java/com/demo/blogrestapi/payload/PostDto.java
+++ src/main/java/com/demo/blogrestapi/payload/PostDto.java
@@ -2,16 +2,23 @@ package com.demo.blogrestapi.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private long id;
 
+    @NotEmpty
+    @Size(min = 2, message = "Title must have at least 2 characters")
     private String title;
 
+    @NotEmpty
+    @Size(min = 5, message = "Description must have at least 5 characters")
     private  String description;
 
+    @NotEmpty
     private String content;
 
     private Set<CommentDto> comments;
