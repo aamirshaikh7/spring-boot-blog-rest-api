0718bddb1844bf93fdc1efe4685420fdb0b9f8e6
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-12 18:50:45 +0530

    Adding spring-security dependency and Implementing basic authentication

---
+++ src/main/java/com/demo/blogrestapi/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.demo.blogrestapi.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
