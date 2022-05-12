8a81c26a49ead8c90705b7d9ab44091f293d6bfa
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-12 21:31:23 +0530

    Implementing In-memory Authentication - Role based

--- src/main/java/com/demo/blogrestapi/controller/PostController.java
+++ src/main/java/com/demo/blogrestapi/controller/PostController.java
@@ -6,6 +6,7 @@ import com.demo.blogrestapi.service.PostService;
 import com.demo.blogrestapi.utils.Constants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,6 +20,7 @@ public class PostController {
         this.postService = postService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost (@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -39,11 +41,13 @@ public class PostController {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<PostDto> updatePost (@Valid @RequestBody PostDto postDto, @PathVariable("id") long id) {
         return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deletePostById (@PathVariable("id") long id) {
         postService.deletePostById(id);
