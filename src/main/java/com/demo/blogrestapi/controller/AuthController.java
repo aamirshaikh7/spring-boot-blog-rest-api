8f9bdeab8a2c853f1ca69aa77470ff5fc77e8d07
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-13 21:16:14 +0530

    Creating Signup Rest API

--- src/main/java/com/demo/blogrestapi/controller/AuthController.java
+++ src/main/java/com/demo/blogrestapi/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.demo.blogrestapi.controller;
 
+import com.demo.blogrestapi.model.Role;
+import com.demo.blogrestapi.model.User;
 import com.demo.blogrestapi.payload.SigninDto;
+import com.demo.blogrestapi.payload.SignupDto;
+import com.demo.blogrestapi.repository.RoleRepository;
+import com.demo.blogrestapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +13,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("api/auth")
@@ -19,13 +27,46 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("signin")
-    public ResponseEntity<String> login (@RequestBody SigninDto signinDto) {
+    public ResponseEntity<String> signin (@RequestBody SigninDto signinDto) {
         Authentication authentication = authenticationManager.
                 authenticate(new UsernamePasswordAuthenticationToken(signinDto.getUsernameOrEmail(), signinDto.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return new ResponseEntity<>("User logged-in successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("signup")
+    public ResponseEntity<?> signup (@RequestBody SignupDto signupDto) {
+        if (userRepository.existsByUsername(signupDto.getUsername())) {
+            return new ResponseEntity<>("Username is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signupDto.getEmail())) {
+            return new ResponseEntity<>("Email is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setName(signupDto.getName());
+        user.setUsername(signupDto.getUsername());
+        user.setEmail(signupDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signupDto.getPassword()));
+
+        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/demo/blogrestapi/payload/SignupDto.java
@@ -0,0 +1,14 @@
+package com.demo.blogrestapi.payload;
+
+import lombok.Data;
+
+@Data
+public class SignupDto {
+    private String name;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+}
