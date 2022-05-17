d0a630e59130b7bf8699503db74ab64db083c4bb
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-17 14:01:30 +0530

    Integrating Swagger

--- src/main/java/com/demo/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/demo/blogrestapi/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                 .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/v3/api-docs/**").permitAll()
                 .anyRequest()
                 .authenticated();
 
