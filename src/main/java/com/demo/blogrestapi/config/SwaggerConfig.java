756e821726743bd12291fe7a67b40f4cbb340dc3
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-17 19:45:21 +0530

    Configuring Swagger UI to include JWT Authorization

--- src/main/java/com/demo/blogrestapi/config/SwaggerConfig.java
+++ src/main/java/com/demo/blogrestapi/config/SwaggerConfig.java
@@ -4,15 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey () {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
     private ApiInfo apiInfo () {
         return  new ApiInfo(
                 "Spring Boot Blog Rest API",
@@ -30,9 +38,25 @@ public class SwaggerConfig {
     public Docket api () {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private SecurityContext securityContext () {
+        return SecurityContext.builder().securityReferences(securityReferenceList()).build();
+    }
+
+    private List<SecurityReference> securityReferenceList () {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+
+        authorizationScopes[0] = authorizationScope;
+
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 }
