53ba3771564d2e511374b2efdec98a7478e4d1f1
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-17 19:05:10 +0530

    Whitelisting Swagger UI URLs

--- src/main/java/com/demo/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/demo/blogrestapi/config/SecurityConfig.java
@@ -52,6 +52,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                 .antMatchers("/api/v1/auth/**").permitAll()
                 .antMatchers("/v3/api-docs/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/webjars/**").permitAll()
                 .anyRequest()
                 .authenticated();
 
