47fe0327b8d526a5f74dec079169664783faa8cb
Aamir Shaikh <aamirshaikh3232@outlook.com>
2022-11-23 12:08:35 +0530

    Insert role metadeta in table

--- src/main/java/com/demo/blogrestapi/BlogRestApiApplication.java
+++ src/main/java/com/demo/blogrestapi/BlogRestApiApplication.java
@@ -1,12 +1,18 @@
 package com.demo.blogrestapi;
 
+import com.demo.blogrestapi.model.Role;
+import com.demo.blogrestapi.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogRestApiApplication {
+public class BlogRestApiApplication implements CommandLineRunner {
+    public BlogRestApiApplication(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
     @Bean
     public ModelMapper modelMapper () {
@@ -17,4 +23,16 @@ public class BlogRestApiApplication {
         SpringApplication.run(BlogRestApiApplication.class, args);
     }
 
+    private final RoleRepository roleRepository;
+    @Override
+    public void run(String... args) {
+        Role admin = new Role();
+        Role user = new Role();
+
+        admin.setName("ROLE_ADMIN");
+        user.setName("ROLE_USER");
+
+        roleRepository.save(admin);
+        roleRepository.save(user);
+    }
 }
